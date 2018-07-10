f1e197c2f9382ded7d46712d7fa9f9d77d840f90
AvinashKaur <avinashkaur520@gmail.com>
2018-07-10 17:51:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mypc/carouselslider/ImageDetailsActivity.java
@@ -0,0 +1,39 @@
+package com.example.mypc.carouselslider;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class ImageDetailsActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Button button;
+    private static final String DRAWABLE_RESOURE = "resource";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_image_details);
+
+        imageView = (ImageView)findViewById(R.id.img);
+        button = (Button)findViewById(R.id.btnClose);
+
+        int drawbleResource = getIntent().getIntExtra(DRAWABLE_RESOURE, 0);
+        imageView.setImageResource(drawbleResource);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/mypc/carouselslider/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.mypc.carouselslider;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static int LOOPS = 1000;
+    public CarouselPagerAdapter adapter;
+    public ViewPager pager;
+    public static int count = 4; //ViewPager items size
+    /**
+     * You shouldn't define first page = 0.
+     * Let define firstpage = 'number viewpager size' to make endless carousel
+     */
+    public static int FIRST_PAGE = 4;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        pager = (ViewPager) findViewById(R.id.myviewpager);
+
+        //set page margin between pages for viewpager
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int pageMargin = ((metrics.widthPixels / 4) * 2);
+        pager.setPageMargin(-pageMargin);
+
+        adapter = new CarouselPagerAdapter(this, getSupportFragmentManager());
+        pager.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+        pager.addOnPageChangeListener(adapter);
+
+        // Set current item to the middle page so we can fling to both
+        // directions left and right
+        pager.setCurrentItem(FIRST_PAGE);
+        pager.setOffscreenPageLimit(3);
+
+
+    }
+}
